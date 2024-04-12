6972d4874ef9927eddefb88c2c6a53e7b77f0f3b
Ashwini-Kanagaraj <159413670+Ashwini-Kanagaraj@users.noreply.github.com>
2024-04-12 11:47:29 +0530

    sns topic

--- main.tf
+++ main.tf
@@ -1,31 +1,19 @@
-module "kms_key" {
-  source                  = "./modules/aws_kms"
+module "Kms_module" {
+  source = "git::https://github.com/Digidense/terraform_module.git//kms?ref=feature/DD-35/kms_module"
+  aliases_name            = "alias/Kms_Demo_Module"
+  description             = "kms module attachment"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
 }
-
+# Creating the sns topic
 resource "aws_sns_topic" "Demo_Topic" {
-  name                       = "Demo_Topic"
-  kms_master_key_id          = module.kms_key.kms_key_arn
-
+  name              = var.sns_name
+  kms_master_key_id = module.Kms_module.kms_key_arn
 }
 
 # Create an email subscription to the SNS topic
 resource "aws_sns_topic_subscription" "Demo_Subscription" {
   topic_arn = aws_sns_topic.Demo_Topic.arn
   protocol  = "email"
-  endpoint  = "ashwinikanagaraj3@gmail.com"  # Replace this with your email address
+  endpoint  = var.endpoint
 }
-
-
-
-
-## Create an SNS topic
-#resource "aws_sns_topic" "Demo_Topic" {
-#  name = "Demo_Topic"
-#}
-#
-## Create an email subscription to the SNS topic
-#resource "aws_sns_topic_subscription" "Demo_Subscription" {
-#  topic_arn = aws_sns_topic.Demo_Topic.arn
-#  protocol  = "email"
-#  endpoint  = "ashwinikanagaraj3@gmail.com"  # Replace this with your email address
-#}
