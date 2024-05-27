986c0b6bb9b0ad1af16b4249dc36b46abc91d1b5
Ashwini-Kanagaraj <159413670+Ashwini-Kanagaraj@users.noreply.github.com>
2024-05-27 11:26:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "cluster_modules" {
-  source                      = "./modules/Cluster_module"
+  source                      = "git::https://github.com/Digidense/terraform_module.git//eks?ref=v3.0.0"
   role_name                   = var.role_name
   node_attachment_name        = var.node_attachment_name
   eks_cluster_attachment_name = var.eks_cluster_attachment_name
