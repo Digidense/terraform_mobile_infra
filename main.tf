a8b4d3c80c4324a7f9ce64623592fdbaf0c1925e
Akhileshml <129209611+Akhileshml@users.noreply.github.com>
2024-05-28 16:28:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "API_Module" {
   source               = "git::https://github.com/Digidense/terraform_module.git//API?ref=V5.0.0"
-  lambda_zip_file      = var.lambda_function_name
+  lambda_zip_file      = var.var.lambda_zip_file
   lambda_handler       = var.lambda_handler
   lambda_runtime       = var.lambda_runtime
   lambda_function_name = var.lambda_function_name
